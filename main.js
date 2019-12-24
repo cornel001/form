f65c4735a95bcd23c0a9b60e856fef4c7dc34e46
Cornel <44235575+cornel001@users.noreply.github.com>
2019-12-24 11:26:35 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//constants for css classes
+
+const selected = '.selected'
+const group = '.group'
+
+//group behaviour
+
+const groups = document.querySelectorAll(group);
+Array.prototype.forEach.call(groups, assignSelectEvent);
+
+function assignSelectEvent(element) {
+  element.addEventListener('click', selectOption);
+}
+
+function selectOption() {
+  const current = event.currentTarget
+  const original = event.target;
+  if (original == current) return
+  if (current.matches(".single") && !original.matches(selected)) {
+    const other = current.querySelector(selected);
+    if (other) other.classList.toggle(selected.slice(1));
+  }
+  original.classList.toggle(selected.slice(1));
+}
+
+//submit behaviour
+
+const submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', submitData);
+
+async function submitData() {
+  const dataToSend = new FormData(document.querySelector('#data-to-send'));
+  const selects = document.querySelectorAll(group + ' > ' + selected);
+
+  for (let select of selects) {
+    dataToSend.append(select.parentElement.id, select.innerHTML)
+  }
+
+  const response = await fetch('to_be_specified.test', {
+    method: 'POST',
+    body: dataToSend
+  });
+  const response = await response.json();
+
+  alert(result.message);
+  // for (let entry of dataToSend)
+  //   console.log(entry)
+}
